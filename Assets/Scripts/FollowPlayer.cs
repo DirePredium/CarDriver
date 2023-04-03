a69201e48ccbb117e279d43dbc2987d9df99a46a
DirePredium <user83054@gmail.com>
2023-04-03 23:51:35 +0300

    Finish first game prototype about car driving

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject gameObject;
+    private Vector3 toForward = new Vector3(0, 5, -7);
+    private Vector3 toBack = new Vector3(0, 5, -11);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.position = gameObject.transform.position + toBack;
+            return;
+        }
+        transform.position = gameObject.transform.position + toForward;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 11.0f;
+    public float turnSpeed = 25.0f;
+    public float horizontalInput;
+    public float forwardInput;
+    public float jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        jump = Input.GetAxis("Jump");
+
+        if (Input.GetAxis("Jump") == 1 && forwardInput < 1 && forwardInput > 0)
+        {
+            transform.Rotate(Vector3.right, -100 * Time.deltaTime);
+        }
+
+        if (forwardInput < 0)
+            horizontalInput *= -1;
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        if(forwardInput != 0)
+            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
